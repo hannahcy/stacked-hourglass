265da9f35e967a3f94685265c2501950854c1fad
Hannah CY <hannah.cy@gmail.com>
2018-06-29 13:03:39 +1200

    f

--- hourglass_tiny.py
+++ hourglass_tiny.py
@@ -292,7 +292,7 @@ class HourglassModel():
 				self.resume['loss'].append(cost)
 				# Validation Set
 				accuracy_array = np.array([0.0]*len(self.joint_accur))
-				temp = tf.reshape(self.get_output(),1,64,64)
+				temp = tf.reshape(self.get_output(),[1,64,64])
 				print(temp)
 				for i in range(validIter):
 					img_valid, gt_valid, w_valid = next(self.generator)
