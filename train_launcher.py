b5054a58644a158e86d7c7b2cf5bd82f40497491
Hannah <hannah.cy@gmail.com>
2018-07-09 16:52:02 +1200

    heatmaps

--- train_launcher.py
+++ train_launcher.py
@@ -34,19 +34,19 @@ def process_config(conf_file):
 
 
 if __name__ == '__main__':
-	print('--Parsing Config File')
-	params = process_config('config.cfg')
+	#print('--Parsing Config File')
+	#params = process_config('config.cfg')
 	
-	print('--Creating Dataset')
-	dataset = DataGenerator(params['joint_list'], params['img_directory'], params['training_txt_file'], remove_joints=params['remove_joints'])
-	dataset._create_train_table()
-	dataset._randomize()
-	dataset._create_sets()
-	#dataset.test()
+	#print('--Creating Dataset')
+	#dataset = DataGenerator(params['joint_list'], params['img_directory'], params['training_txt_file'], remove_joints=params['remove_joints'])
+	#dataset._create_train_table()
+	#dataset._randomize()
+	#dataset._create_sets()
+	##dataset.test()
 	
-	model = HourglassModel(nFeat=params['nfeats'], nStack=params['nstacks'], nModules=params['nmodules'], nLow=params['nlow'], outputDim=params['num_joints'], batch_size=params['batch_size'], attention = params['mcam'],training=True, drop_rate= params['dropout_rate'], lear_rate=params['learning_rate'], decay=params['learning_rate_decay'], decay_step=params['decay_step'], dataset=dataset, name=params['name'], logdir_train=params['log_dir_train'], logdir_test=params['log_dir_test'], tiny= params['tiny'], w_loss=params['weighted_loss'] , joints= params['joint_list'],modif=False)
-	model.generate_model()
-	model.training_init(nEpochs=params['nepochs'], epochSize=params['epoch_size'], saveStep=params['saver_step'], dataset = None)
+	#model = HourglassModel(nFeat=params['nfeats'], nStack=params['nstacks'], nModules=params['nmodules'], nLow=params['nlow'], outputDim=params['num_joints'], batch_size=params['batch_size'], attention = params['mcam'],training=True, drop_rate= params['dropout_rate'], lear_rate=params['learning_rate'], decay=params['learning_rate_decay'], decay_step=params['decay_step'], dataset=dataset, name=params['name'], logdir_train=params['log_dir_train'], logdir_test=params['log_dir_test'], tiny= params['tiny'], w_loss=params['weighted_loss'] , joints= params['joint_list'],modif=False)
+	#model.generate_model()
+	#model.training_init(nEpochs=params['nepochs'], epochSize=params['epoch_size'], saveStep=params['saver_step'], dataset = None)
 	
-	inference = Inference(model='hg_refined_200_200')
+	inference = Inference(model='hg_refined_200_60')
 	inference.predictHM('08000.jpgA')
