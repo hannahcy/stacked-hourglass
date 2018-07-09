5251d61ce27fa1c950a61a9874265630b60a1b54
Hannah <hannah.cy@gmail.com>
2018-07-09 18:54:45 +1200

    heatmaps

--- generate_heatmap.py
+++ generate_heatmap.py
@@ -4,7 +4,7 @@ import numpy as np
 
 infer=Inference(model='hg_refined_200_60')
 
-img=cv2.imread("dataMarsden25-25-SPLIT/08000.jpg")
+img=cv2.imread("dataMarsden25-25-SPLIT/00000.jpg")
 
 img = cv2.resize(img, (128, 128))
 
@@ -17,4 +17,4 @@ for i in range(np.shape(hms)[3]):
     x=index%64*4
     y=int(index/64)*4
     new_img=cv2.circle(new_img,(x,y),3,(0,0,255),-1)
-cv2.imwrite("heatmap08000.jpg",new_img)
+cv2.imwrite("heatmap00000.jpg",new_img)
